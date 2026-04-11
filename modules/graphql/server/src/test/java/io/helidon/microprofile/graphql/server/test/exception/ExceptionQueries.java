73319a895cf6e734f4ad92f6e5dede67ba2a307c
Tomas Langer <tomas.langer@oracle.com>
2026-04-11 17:39:22 +0200

    graphql-server: replace AccessControlException

--- modules/graphql/server/src/test/java/io/helidon/microprofile/graphql/server/test/exception/ExceptionQueries.java
+++ modules/graphql/server/src/test/java/io/helidon/microprofile/graphql/server/test/exception/ExceptionQueries.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2025 Oracle and/or its affiliates.
+ * Copyright (c) 2020, 2026 Oracle and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@
 package io.helidon.microprofile.graphql.server.test.exception;
 
 import java.io.IOException;
-import java.security.AccessControlException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -52,12 +51,12 @@ public class ExceptionQueries {
 
     @Query("uncheckedQuery1")
     public String uncheckedQuery1() {
-        throw new IllegalArgumentException(new AccessControlException("my exception"));
+        throw new IllegalArgumentException(new SecurityException("my exception"));
     }
 
     @Query("uncheckedQuery2")
     public String uncheckedQuery2() {
-        throw new MyIllegalArgumentException(new AccessControlException("my exception"));
+        throw new MyIllegalArgumentException(new SecurityException("my exception"));
     }
 
     @Query
